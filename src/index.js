747a8f8977f0c82226d2393ce04d2b848b98abc8
mintranno1 <minhtranhcm.mt@gmail.com>
2021-09-20 13:44:44 +0700

    su dung lint-staged -> de khi chay git add . -> sau do chay npm run formatcode -> nó se su lai code tat ca nhung file git add bi sai cu phap -> theo duong dan tai pakage.json -> lint-staged : {....} + formatcode : lint-staged -> viec chay git add . xong chay npm run formatcode van con thu cong nen phai dung toi husky

--- src/index.js
+++ src/index.js
@@ -16,15 +16,16 @@ var morgan = require('morgan');
 
 // Template engine
 app.engine(
-    'hbs',
-    handlebars({
-        extname: '.hbs',
+    "hbs",
+    							handlebars({
+        			extname: '.hbs',
     }),
 );
-app.set('view engine', 'hbs');
+app.set(  				'view engine', 'hbs');
 
 // Set src view static
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', 
+path.join(__dirname, 'resources/views'));
 
 //Routes init
 route(app); //goi tu file index -> folder routes
